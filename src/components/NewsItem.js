a3f6f68091669a06aa3c4c56bc8dc4f93fa63841
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2021-11-19 13:49:16 +0530

    added api for top news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,7 @@ export default class NewsItem extends Component {
             <div className="col-md-4">
                  
             <div className="card m-2" style={{width: "18rem"}}>
-                <img className="card-img-top" src={imageUrl} alt="Img not found"/>
+                <img className="card-img-top" src={imageUrl?imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfTftcosmlVzliw9GOVXJs8cbcUXt6pmQZbQ&usqp=CAU"} alt="Img not found"/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
